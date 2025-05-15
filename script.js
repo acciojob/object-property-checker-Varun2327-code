da396b1d237b96b32a50c4304dbf12b3603c35f7
Varun shrimal <varunshrimal07@gmail.com>
2025-05-15 07:48:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+// Global object
+const sampleObject = {
+  red: "#FF0000",
+  green: "#00FF00",
+  white: "#FFFFFF"
+};
 
+// Function to check if the key exists
 function hasKey(key) {
-  //   write your code here
+  return key in sampleObject;
 }
-
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
